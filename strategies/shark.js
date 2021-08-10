69758ceeb99c642f54df6653068b77a84fa3cb89
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-10 18:08:45 +0100

    fixing shark strategy

--- strategies/shark.js
+++ strategies/shark.js
@@ -1,10 +1,10 @@
 const rsi = require('../indicators/rsi.js')
 const EMA = require('../indicators/ema.js')
 const tools = require('../tools/index')
+const stoch = require('../indicators/stoch.js')
 const CANDLE = require('../tools/constants').CANDLE
 const STRATEGIES = require('../tools/constants').STRATEGIES
 const POSITION = require('../tools/constants').POSITION_SIDE
-const hasCrossStoch = require('../tools/validations').hasCrossStoch
 
 const periodTime = '5m'
 const rsiPeriod = 3// 80 - 20
@@ -38,6 +38,31 @@ function validateEntry (candles) {
       console.log('SAIDA 1.5 - Erro ao setar stop price and target price ')
       return false
     }
+  }
+}
+function hasCrossStoch (candles, stochPeriod) {
+  const stochArray = stoch.checkingStoch(candles, stochPeriod)
+  const lastTwoStoch = tools.getLasts(stochArray, 2)
+  const lastK = lastTwoStoch[1].k
+  const beforeK = lastTwoStoch[0].k
+  const lastD = lastTwoStoch[1].d
+  const beforeD = lastTwoStoch[0].d
+  const crossDown = lastK <= lastD && beforeK > beforeD ? POSITION.SHORT : false
+  const crossUp = lastK >= lastD && beforeK < beforeD ? POSITION.LONG : false
+  const kOver80 = lastK > 80 || beforeK > 80
+  const dOver80 = lastD > 80 || beforeD > 80
+  const kUnder20 = lastK < 20 || beforeK < 20
+  const dUnder20 = lastD < 20 || beforeD < 20
+
+  if (crossDown) {
+    if (kOver80 && dOver80) return false
+    return crossDown
+  } else if (crossUp) {
+    if (kUnder20 && dUnder20) return false
+    return crossUp
+  } else {
+    console.log('SAIDA 17')
+    return false
   }
 }
 
