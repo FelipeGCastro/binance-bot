685f991fd2e521c27c236755b356c7020c6040b7
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-13 20:55:57 +0100

    comment register route

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -10,20 +10,20 @@ function generateToken (params = {}) {
   return jwt.sign(params, authConfig.secret)
 }
 
-userRoutes.post('/register', async (req, res) => {
-  const { email } = req.body
-  try {
-    if (await User.findOne({ email })) { return res.status(400).send({ error: 'User already exists' }) }
-    const user = await User.create(req.body)
-    user.password = undefined
-    return res.send({
-      user,
-      token: generateToken({ id: user.id })
-    })
-  } catch (error) {
-    return res.status(400).send({ error: 'Registration failed', data: error })
-  }
-})
+// userRoutes.post('/register', async (req, res) => {
+//   const { email } = req.body
+//   try {
+//     if (await User.findOne({ email })) { return res.status(400).send({ error: 'User already exists' }) }
+//     const user = await User.create(req.body)
+//     user.password = undefined
+//     return res.send({
+//       user,
+//       token: generateToken({ id: user.id })
+//     })
+//   } catch (error) {
+//     return res.status(400).send({ error: 'Registration failed', data: error })
+//   }
+// }).use()
 
 userRoutes.post('/authenticate', async (req, res) => {
   const { email, password } = req.body
