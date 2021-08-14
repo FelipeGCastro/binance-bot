641450fbdc6f44898a4d49fae024200f611b0f6b
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-15 00:51:56 +0100

    add dev_env variable

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
-// const mongoUser = process.env.DB_USER
-// const mongoSecret = process.env.DB_SECRET
-// const mongoURI = `mongodb+srv://${mongoUser}:${mongoSecret}@luizbotapi.mzzrx.mongodb.net/luizbot?retryWrites=true&w=majority`
-const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+
+const mongoUser = process.env.DB_USER
+const mongoSecret = process.env.DB_SECRET
+const mongoURI = process.env.DEV_ENV === 'DEV'
+  ? 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+  : `mongodb+srv://${mongoUser}:${mongoSecret}@luizbotapi.mzzrx.mongodb.net/luizbot?retryWrites=true&w=majority`
+
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
