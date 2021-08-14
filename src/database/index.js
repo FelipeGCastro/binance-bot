4fa2c4a6a39b377ee52afc8ee84b6bc679ee3788
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-14 05:11:32 +0100

    start set tracking multiple symbols

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-const mongoUser = process.env.DB_USER
-const mongoSecret = process.env.DB_SECRET
-const mongoURI = `mongodb+srv://${mongoUser}:${mongoSecret}@luizbotapi.mzzrx.mongodb.net/luizbot?retryWrites=true&w=majority`
+// const mongoUser = process.env.DB_USER
+// const mongoSecret = process.env.DB_SECRET
+// const mongoURI = `mongodb+srv://${mongoUser}:${mongoSecret}@luizbotapi.mzzrx.mongodb.net/luizbot?retryWrites=true&w=majority`
+const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
