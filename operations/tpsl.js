fc954c0e7069bcdc709d643aba0cfe7cd2cfe2a2
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-16 19:32:40 +0100

    set messages for failed tpsl

--- operations/tpsl.js
+++ operations/tpsl.js
@@ -1,5 +1,6 @@
 const api = require('../services/api')
 const ORDER_TYPE = require('../tools/constants').ORDER_TYPE
+const telegram = require('../services/telegram')
 const SIDE = require('../tools/constants').SIDE
 
 async function createTpandSLOrder (order) {
@@ -14,11 +15,13 @@ async function createTpandSLOrder (order) {
   const stopOrder = await api.newOrder(symbol, null, side, ORDER_TYPE.STOP_MARKET, true, stopMarketPrice)
   const profitOrder = await api.newOrder(symbol, null, side, ORDER_TYPE.TAKE_PROFIT_MARKET, true, takeProfitPrice)
   if (!stopOrder) {
+    telegram.sendMessage(`Problem ao criar Stop Loss Order para ${symbol}`)
     console.log('Error create stop market order')
     return false
   }
   order.updateTradesOn(symbol, 'stopOrderCreated', true)
   if (!profitOrder) {
+    telegram.sendMessage(`Problem ao criar Take Profit Order para ${symbol}`)
     console.log('Error create take profit order')
     return false
   }
