62120ca2efd6ced5536ba7071969701100c15598
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-19 16:44:55 +0100

    start setting socket and fix accounts orders

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 const express = require('express')
-
+const socketio = require('socket.io')
 const auth = require('./controllers/authControllers')
 const account = require('./controllers/accountControler')
 const tradeRoutes = require('./controllers/tradeController')
 const authMiddleware = require('./middlewares/auth')
+const http = require('http')
 
 const app = express()
+const httpServer = http.createServer(app)
+const io = new socketio.Server(httpServer)
 
 app.use(express.json())
 
 app.use('/user', auth).use(authMiddleware)
 app.use('/account', account).use(authMiddleware)
 app.use('/trade', tradeRoutes).use(authMiddleware)
+
+io.on('connection', (socket) => {
+  console.log(`New conection: ${socket.id}`)
+})
 
 app.listen(process.env.PORT || 3333, () => console.log('Server is running'))
 
