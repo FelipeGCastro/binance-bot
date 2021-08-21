6aaa69505d8c84cd88105d48f24033c62a1812bd
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-22 00:19:27 +0100

    change to verify every updated candle from websocket

--- operations/userDataUpdate.js
+++ operations/userDataUpdate.js
@@ -76,7 +76,7 @@ async function handleFilledOrder (order) {
         console.log('Saida 18 Order Type TPSL FILLED', order.ot)
         return await tpslOrderFilled(order)
       } else {
-        console.log('Saida 19 -Order MARKET Filled with NO position open NO TPSL', order)
+        console.log('Saida 19 -Order MARKET Filled with NO position open NO TPSL')
       }
       order.removeFromTradesOn(order.account, order.symbol)
     } else {
