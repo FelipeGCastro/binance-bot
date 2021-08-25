98dc4cb4e88f692e38d326903e685c8f7992ee4d
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-25 20:03:42 +0100

    first commit

---
+++ src/models/tradeOn.js
@@ -0,0 +1,69 @@
+const mongoose = require('../database')
+
+const TradeOnSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  symbol: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  strategy: {
+    type: String,
+    required: true
+  },
+  stopMarketPrice: {
+    type: String,
+    required: true
+  },
+  takeProfitPrice: {
+    type: String,
+    required: true
+  },
+  entryPrice: {
+    type: String,
+    required: true
+  },
+  breakevenTriggerPrice: {
+    type: String
+  },
+  riseStopTriggerPrice: {
+    type: String
+  },
+  breakevenCreated: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  riseStopCreated: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  stopOrderCreated: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  profitOrderCreated: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  side: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const TradeOn = mongoose.model('TradeOn', TradeOnSchema)
+
+module.exports = { TradeOn, TradeOnSchema }
